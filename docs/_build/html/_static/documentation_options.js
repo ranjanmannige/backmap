dbed9790d3464c47aceeefbc57995d40223cbe23
ranjanmannige <ranjanmannige@gmail.com>
2018-04-13 13:18:08 -0400

    update

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: 'v0.2.2+0.g10c96c0.dirty',
+    VERSION: 'v0.2.3+3.g96bff48.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
