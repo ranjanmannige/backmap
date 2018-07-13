60e658b1c86dcf999aabc1ad81c70b4774af1164
ranjanmannige <ranjanmannige@gmail.com>
2018-07-13 16:33:52 -0400

    Manuscript first draft completed

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: 'v0.2.4+3.gbf4c9ed.dirty',
+    VERSION: 'v0.2.6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
